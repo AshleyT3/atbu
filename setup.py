18ac2671e8dbf415af1030b1f12c9ee0e4318654
Ashley R. Thomas <ashley.r.thomas.701@gmail.com>
2022-07-25 20:40:50 -0700

    Bump setup.py version 0.0.18 -> 0.0.19

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import setuptools
 
 setuptools.setup(
     name="atbu-pkg",
-    version="0.0.18",
+    version="0.0.19",
     author="Ashley R. Thomas",
     author_email="ashley.r.thomas.701@gmail.com",
     description= (
