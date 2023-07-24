0195968b491d21dd8de2dbb81d9f63d400af5693
Ashley R. Thomas <ashley.r.thomas.701@gmail.com>
2023-07-24 12:21:26 -0700

    version 0.0.28

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import setuptools
 
 setuptools.setup(
     name="atbu-pkg",
-    version="0.0.27",
+    version="0.0.28",
     author="Ashley R. Thomas",
     author_email="ashley.r.thomas.701@gmail.com",
     description= (
